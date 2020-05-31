c77445dd2d001d948d6638bb953661196a9f15c6
Olaf Wysocki <olaf.wysocki@tum.de>
2020-05-31 14:06:31 +0200

    Init

---
+++ webpage/app.js
@@ -0,0 +1,98 @@
+
+/*
+const COLOR_RANGE = [
+    [1, 152, 189],
+    [73, 227, 206],
+    [216, 254, 181],
+    [254, 237, 177],
+    [254, 173, 84],
+    [209, 55, 78]
+  ];
+*/
+// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
+//const AIR_PORTS =
+//  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
+
+
+
+// const HomeWins = './HomeWins2019_2020.geojson';
+const HomeWins = './AllSeasons.json';
+
+// create color array for geojson layer
+function createColor (input) {
+    if (input <= 4){
+        colArr = [1, 152, 189]
+    }
+    else if (input > 4 && input <= 12 ) {
+        colArr = [73, 227, 206]
+    }
+    else if (input > 12 && input <= 33 ){
+        colArr = [216, 254, 181]
+    }
+    else if (input > 33 && input <= 57 ){
+        colArr = [216, 254, 181]
+    }
+    else if (input > 57 && input <= 135 ){
+        colArr = [216, 254, 181]
+    }
+    else {
+        colArr = [209, 55, 78]
+    }
+    return colArr
+}
+
+//create pop-up
+function getTooltip({object}) {
+    return object && `Total County Wins:
+  ${object.properties.sumHWins}
+  Teams:
+  ${object.properties.TeamsNames}`;
+  }
+
+//Map
+const deckgl = new deck.DeckGL({
+  container: 'container',
+  // Set your Mapbox access token here
+  mapboxApiAccessToken: 'pk.eyJ1Ijoib2xvb2NraSIsImEiOiJja2E3enBpZG4wODhkMnJydTg5c2V5a280In0.U-h43N-R4QbrEe6J90T6GA',
+  mapStyle: 'mapbox://styles/mapbox/dark-v9',
+
+  initialViewState: {
+    longitude: -1.4157,
+    latitude: 52.2324,
+    zoom: 6,
+    minZoom: 5,
+    maxZoom: 15,
+    pitch: 40.5
+  },
+  controller: true,
+
+  layers: [
+
+//add homeWins as 2D
+    new deck.GeoJsonLayer({
+      // data
+      id: 'homewins',
+      data: HomeWins,
+      
+      // Styles
+      filled: true,
+      extruded: true,
+      wireframe: false,
+      fp64: false,
+      getElevation: d => d.properties.sumHWins * 500,
+      //getFillColor: f => COLOR_SCALE(f.properties.sumHWins),
+      getFillColor: d => createColor(d.properties.sumHWins),
+      getLineColor: d => [255, 255, 255],
+
+      // Interactive props
+      pickable: true,
+      autoHighlight: true,
+      //onClick: info => info.object && alert(`${info.object.properties.sumHWins} (${info.object.properties.TeamsNames}) (${info.object.properties.sumCapacity})`)
+      //onHover: info => setTooltip(info.object, info.x, info.y)
+    })
+
+  ],
+  getTooltip
+
+  
+});
