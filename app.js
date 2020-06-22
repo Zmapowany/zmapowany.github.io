2415c946f36cb29c0de1093012900008854263cb
Olaf Wysocki <olaf.wysocki@tum.de>
2020-06-22 21:09:14 +0200

    phase22

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 
 // const HomeWins = './HomeWins2019_2020.geojson';
-const HomeWins = './data/AllSeasons.json';
+const HomeWins = './AllSeasonsGen2.json';
 
 // create color array for geojson layer
 function createColor (input) {
     if (input <= 4){
-        colArr = [1, 152, 189]
+        colArr = [21, 74, 234]
     }
     else if (input > 4 && input <= 12 ) {
-        colArr = [73, 227, 206]
+        colArr = [43, 131, 186]
     }
     else if (input > 12 && input <= 33 ){
-        colArr = [216, 254, 181]
+        colArr = [171, 221, 164]
     }
     else if (input > 33 && input <= 57 ){
-        colArr = [216, 254, 181]
+        colArr = [255, 255, 191]
     }
     else if (input > 57 && input <= 135 ){
-        colArr = [216, 254, 181]
+        colArr = [253, 174, 97]
     }
     else {
-        colArr = [209, 55, 78]
+        colArr = [215, 25, 28]
     }
     return colArr
 }
@@ -32,8 +32,22 @@ function getTooltip({object}) {
   Teams:
   ${object.properties.TeamsNames}`;
   }
+//shadow
 
-//Map
+/*
+const ambientLight = new deck.AmbientLight({
+  color: [255, 255, 255],
+  intensity: 1.0
+});
+
+const directionalLight = new deck.DirectionalLight({
+  color: [255, 255, 255],
+  direction: [0, 0, -1],
+  intensity: 1,
+  _shadow: false
+});
+*/
+  //Map
 const deckgl = new deck.DeckGL({
   container: 'container',
   // Set your Mapbox access token here
@@ -46,22 +60,33 @@ const deckgl = new deck.DeckGL({
     zoom: 6,
     minZoom: 5,
     maxZoom: 15,
-    pitch: 40.5
+    pitch: 40.5,
+    // bearing: -27.396674584323023
+    bearing: -60
   },
   controller: true,
+  
+  //some lights
+  /*
+  effects: [
+    new deck.LightingEffect({directionalLight})
+  ],
+*/
+
 
   layers: [
 
-//add homeWins as 2D
+//add homeWins
     new deck.GeoJsonLayer({
       // data
       id: 'homewins',
       data: HomeWins,
       
       // Styles
+      opacity: 0.9,
       filled: true,
       extruded: true,
-      wireframe: false,
+      wireframe: true,
       fp64: false,
       getElevation: d => d.properties.sumHWins * 500,
       getFillColor: d => createColor(d.properties.sumHWins),
