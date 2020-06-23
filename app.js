29189cfd6b758ebcc6eab4877eff85f171bb6b93
Olaf Wysocki <olaf.wysocki@tum.de>
2020-06-23 16:46:19 +0200

    Button

--- app.js
+++ app.js
@@ -1,6 +1,118 @@
 
-// const HomeWins = './HomeWins2019_2020.geojson';
+//data
 const HomeWins = './AllSeasonsGen2.json';
+const stadiums = './HomeWinsAllSeasons.geojson';
+
+
+//toggle on and off
+function onoffBournemouth(){
+  currentvalue = document.getElementById('onoff').value;
+  if(currentvalue == "3D Off"){
+    //change button value to ON -> turn ON this layer
+    document.getElementById("onoff").value="3D On";
+    onoff.style.opacity = 1; 
+    
+    deckgl.setProps({layers: [
+      new deck.GeoJsonLayer({
+        id: 'stadiums',
+        data: stadiums,
+        visible: false,
+        // Styles
+        filled: true,
+        pointRadiusMinPixels: 2,
+        pointRadiusScale: 1000,
+        //getRadius: 1000,
+        getFillColor: [209, 29, 39],
+        // Interactive props
+        pickable: false,
+        autoHighlight: true,
+        onClick: info =>
+          // eslint-disable-next-line
+          info.object && alert(`Team: ${info.object.properties.Team} \n Nr of Nations:  (${info.object.properties.nrOfNations}) \n List of Nations: (${info.object.properties.listOfNations}) \n List of Nations and Players: (${info.object.properties.listOfNationsPlayers})`)
+          
+      }),
+      //LAYERs #2 -- clubs players and their home locations
+      new deck.GeoJsonLayer({
+        // data
+        id: 'homewins',
+        data: HomeWins,
+        visible: true,
+        // Styles
+        opacity: 0.9,
+        filled: true,
+        extruded: true,
+        wireframe: true,
+        fp64: false,
+        getElevation: d => d.properties.sumHWins * 500,
+        getFillColor: d => createColor(d.properties.sumHWins),
+        //getLineColor: d => [255, 255, 255],
+        getLineColor: d => [0, 0, 0],
+  
+        // Interactive props
+        pickable: true,
+        autoHighlight: true,
+        //onClick: info => info.object && alert(`${info.object.properties.sumHWins} (${info.object.properties.TeamsNames}) (${info.object.properties.sumCapacity})`)
+        //onHover: info => setTooltip(info.object, info.x, info.y)
+      })
+    ]
+  })
+//toggle OFF
+  }
+  else{
+    //change button value to OFF -> turn OFF this layer
+    document.getElementById("onoff").value="3D Off";
+    onoff.style.opacity = 0.5;
+
+    deckgl.setProps({layers: [
+      new deck.GeoJsonLayer({
+        id: 'stadiums',
+        data: stadiums,
+        visible: true,
+        // Styles
+        filled: true,
+        pointRadiusMinPixels: 2,
+        pointRadiusScale: 1000,
+        //getRadius: 1000,
+        getFillColor: [209, 29, 39],
+        // Interactive props
+        pickable: false,
+        autoHighlight: true,
+        onClick: info =>
+          // eslint-disable-next-line
+          info.object && alert(`Team: ${info.object.properties.Team} \n Nr of Nations:  (${info.object.properties.nrOfNations}) \n List of Nations: (${info.object.properties.listOfNations}) \n List of Nations and Players: (${info.object.properties.listOfNationsPlayers})`)
+          
+      }),
+      //LAYERs #2 -- clubs players and their home locations
+
+      new deck.GeoJsonLayer({
+        // data
+        id: 'homewins',
+        data: HomeWins,
+        visible: false,
+        // Styles
+        opacity: 0.9,
+        filled: true,
+        extruded: true,
+        wireframe: true,
+        fp64: false,
+        getElevation: d => d.properties.sumHWins * 500,
+        getFillColor: d => createColor(d.properties.sumHWins),
+        //getLineColor: d => [255, 255, 255],
+        getLineColor: d => [0, 0, 0],
+  
+        // Interactive props
+        pickable: true,
+        autoHighlight: true,
+        //onClick: info => info.object && alert(`${info.object.properties.sumHWins} (${info.object.properties.TeamsNames}) (${info.object.properties.sumCapacity})`)
+        //onHover: info => setTooltip(info.object, info.x, info.y)
+      })
+    ]
+  })
+  }
+}
+
+
+
 
 // create color array for geojson layer
 function createColor (input) {
@@ -32,9 +144,11 @@ function getTooltip({object}) {
   Teams:
   ${object.properties.TeamsNames}`;
   }
-//shadow
 
-/*
+  //make artificial ground for shadows 
+  //const landCover = [[[-5.840542, 49.255583], [-5.840542, 58.207266], [5.183309, 58.207266], [5.183309, 49.196]]];
+
+  //lighting effects
 const ambientLight = new deck.AmbientLight({
   color: [255, 255, 255],
   intensity: 1.0
@@ -42,17 +156,20 @@ const ambientLight = new deck.AmbientLight({
 
 const directionalLight = new deck.DirectionalLight({
   color: [255, 255, 255],
-  direction: [0, 0, -1],
-  intensity: 1,
+  //direction: [0, 0, -1],
+  direction: [0.5, 5, -5],
+  intensity: 1.5,
   _shadow: false
 });
-*/
+
+
   //Map
 const deckgl = new deck.DeckGL({
   container: 'container',
   // Set your Mapbox access token here
   mapboxApiAccessToken: 'pk.eyJ1Ijoib2xvb2NraSIsImEiOiJja2E3enBpZG4wODhkMnJydTg5c2V5a280In0.U-h43N-R4QbrEe6J90T6GA',
-  mapStyle: 'mapbox://styles/mapbox/dark-v9',
+  //mapStyle: 'mapbox://styles/mapbox/dark-v9',
+  mapStyle: 'mapbox://styles/mapbox/light-v10',
 
   initialViewState: {
     longitude: -1.4157,
@@ -67,16 +184,27 @@ const deckgl = new deck.DeckGL({
   controller: true,
   
   //some lights
-  /*
+  
   effects: [
-    new deck.LightingEffect({directionalLight})
+    new deck.LightingEffect({directionalLight, ambientLight})
   ],
-*/
+
 
 
   layers: [
 
 //add homeWins
+
+//in case shadow needed
+/*
+new deck.PolygonLayer({
+  id: 'ground',
+  data: landCover,
+  stroked: false,
+  getPolygon: f => f,
+  getFillColor: [0, 0, 0, 0]
+}),
+*/
     new deck.GeoJsonLayer({
       // data
       id: 'homewins',
@@ -90,7 +218,8 @@ const deckgl = new deck.DeckGL({
       fp64: false,
       getElevation: d => d.properties.sumHWins * 500,
       getFillColor: d => createColor(d.properties.sumHWins),
-      getLineColor: d => [255, 255, 255],
+      //getLineColor: d => [255, 255, 255],
+      getLineColor: d => [0, 0, 0],
 
       // Interactive props
       pickable: true,
