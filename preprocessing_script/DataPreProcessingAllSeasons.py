418124b2dcc3bf0f5e335f7803fef65d493d56b1
Olaf Wysocki <olaf.wysocki@tum.de>
2020-07-07 13:27:44 +0200

    Last2

--- preprocessing_script/DataPreProcessingAllSeasons.py
+++ preprocessing_script/DataPreProcessingAllSeasons.py
@@ -7,16 +7,30 @@ Created on Fri May 22 10:57:37 2020
 
 import pandas as pd
 import geopandas
+import glob
+
 
 ###Files import
+path = './PLdata' # use your path
+all_files = glob.glob(path + "/*.csv")
+
+li = []
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    li.append(df)
+
+allSeasons = pd.concat(li, axis=0, ignore_index=True)
+
+
 #import current season of the PL 
-seas1 = pd.read_csv("./E0.csv")
+#seas = pd.read_csv("./E0.csv")
 #import WGS84 location of stadiums
-stadiums = pd.read_csv("./stadiums-with-GPS-coordinates.csv")
+stadiums = pd.read_csv("./stadiums.csv")
 #import NUTS2 regions
 #nuts2 = geopandas.read_file('./NUTS2_countiesUK.json')
 
-#select only stadiums in England (and Wales)
+#select only stadiums in England
 stadEngland = stadiums[stadiums["Country"] == 'England']
 
 
@@ -25,7 +39,7 @@ stadEngland = stadiums[stadiums["Country"] == 'England']
 ####First IDEA#####
 # "Home Fortress - count most home wins per team"
 
-hWins = seas1[seas1["FTR"] == 'H'] #select only home wins
+hWins = allSeasons[allSeasons["FTR"] == 'H'] #select only home wins
 hWinsC = hWins["HomeTeam"].value_counts() #count home wins
 # convert to dataframe 
 hWinsCount = hWinsC.to_frame() #come back to data frame
@@ -36,8 +50,8 @@ hWinsCount['key'] = hWinsCount.index
 winHomTeamLoc = pd.merge(hWinsCount, stadEngland, how='left', left_on='key', right_on='FDCOUK') #left join
 
 #Save as csv
-winHomTeamLoc.to_csv('HomeWins2019_2020.csv', index=False)
+winHomTeamLoc.to_csv('HomeWinsAllSeasons.csv', index=False)
 
 #save as geoDataFrame
 gdf = geopandas.GeoDataFrame(winHomTeamLoc, geometry=geopandas.points_from_xy(winHomTeamLoc.Longitude, winHomTeamLoc.Latitude))
-gdf.to_file("HomeWins2019_2020.geojson", driver='GeoJSON')
+gdf.to_file("HomeWinsAllSeasons.geojson", driver='GeoJSON')
